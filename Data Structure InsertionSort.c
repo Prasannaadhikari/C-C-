c5cb7ecd1817c100513be945fc0725ad3ac146ff
Prasanna Adhikari <36228386+Prasannaadhikari@users.noreply.github.com>
2019-07-09 17:09:38 +0545

    Add files via upload

---
+++ Data Structure InsertionSort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int a[10],n,i,hole,val;
+    printf("Enter the number of data to be inserted: ");
+    scanf("%d",&n);
+    printf("Enter the data to be sorted:  ");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++){
+        val = a[i];
+        hole = i;
+        while(hole>0 && a[hole-1]>val){
+            a[hole] = a[hole-1];
+            --hole;
+        }
+        a[hole] = val;
+    }
+    printf("\nThe data sorted is : ");
+    for(i=0;i<n;i++){
+        printf("%d  ",a[i]);
+    }
+    return 0;
+}
